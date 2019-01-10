476d50fb39414e7e1c4c1651a5889b28af2528a9
kennypee <ekeneeze3@gmail.com>
2019-01-10 16:28:09 +0100

    complete project

---
+++ server.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Scaledrone = require('scaledrone-node-push');
+const app = express();
+const port = process.env.PORT || 4000;
+const cors = require("cors");
+  
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use((req, res) => {
+    res.send(req.body)
+  });
+  
+  app.post('/vote', (req, res) => { 
+    console.log(req.body.playerId)
+
+    const sd = new Scaledrone({
+    channelId: 'giJSOEI9gpOCt4Nx',
+    secretKey: 'IGQustebrgl8Y0Xd78uL9lVtnWJEu0Gf'
+    });
+
+     sd.publish({
+         room: "live-votes",
+         message: req.body.playerId
+     })
+
+     res.json({
+         vote: req.body.playerId
+  })
+
+
+});
+  app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Player from './components/players'
+import cr7 from '../src/img/ronaldo.jpg'
+import lm10 from '../src/img/lm10.jpg'
+import pogba from '../src/img/paul.jpg';
+import axios from 'axios';
+  const playerData = [
+    {
+      name: 'Ronaldo',
+      goals: 30,
+      votes: 0,
+      id: 1,
+      image: cr7
+    },
+    {
+      name: 'Messi',
+      goals: 8,
+      votes: 0,
+      id: 2,
+      image: lm10
+    },
+    {
+      name: 'Pogba',
+      goals: 26,
+      votes: 0,
+      id: 3,
+      image: pogba
+    }
+  ];
+  
+  class App extends Component {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+constructor() {
+  super();
+  this.drone = new window.Scaledrone('giJSOEI9gpOCt4Nx');
+  this.drone.on('open', error => {
+    if (error) {
+      return console.error("Error");
+    }
+  })
+  const room = this.drone.subscribe('live-votes');
+  room.on('data', (data) => {
+    this.state.playerDetails.map(player => {
+      if (player.name === data) {
+        return Object.assign({}, player, {
+          votes: player.votes += 5
+        });
+      } else {
+        return player;
+      }
+    });
+    this.setState({
+      playerDetails: this.state.playerDetails
+    });
+  });
 }
+  state = {
+    playerDetails: []
+  }
+  
+  componentDidMount() {
+      this.setState({ playerDetails: playerData })
+    }
 
-export default App;
+  handleEvent = playerId => {
+ 
+    const data = { playerId }
+    axios.post('http://localhost:4000/vote', data)
+    .then(data => {
+      console.log(data)
+      this.drone.publish({
+        room: "live-votes",
+        message: data.data.playerId
+    })
+   })
+  }
+    
+    render() {
+      return playerData.map(player =>
+        <Player
+          key={player.id}
+          name={player.name}
+          goals={player.goals}
+          image={player.image}
+          votes={player.votes}
+          id={player.id}
+          onVoteCasted={this.handleEvent}
+        />);
+    }
+  }
+  export default App;
