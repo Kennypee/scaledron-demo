a47c746b54120f18843011a2f0c8a5e344614ffe
kennypee <ekeneeze3@gmail.com>
2019-01-12 09:26:03 +0100

    update server

--- server.js
+++ server.js
@@ -5,33 +5,30 @@ const Scaledrone = require('scaledrone-node-push');
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
-  
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use((req, res) => {
-    res.send(req.body)
-  });
-  
-  app.post('/vote', (req, res) => { 
-    console.log(req.body.playerId)
 
-    const sd = new Scaledrone({
-    channelId: 'giJSOEI9gpOCt4Nx',
-    secretKey: 'IGQustebrgl8Y0Xd78uL9lVtnWJEu0Gf'
-    });
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-     sd.publish({
-         room: "live-votes",
-         message: req.body.playerId
-     })
 
-     res.json({
-         vote: req.body.playerId
+const sd = new Scaledrone({
+  channelId: 'giJSOEI9gpOCt4Nx',
+  secretKey: 'IGQustebrgl8Y0Xd78uL9lVtnWJEu0Gf'
+});
+
+app.post('/vote', (req, res) => {
+
+  sd.publish({
+    room: "live-votes",
+    message: req.body.playerId
+  })
+
+  res.json({
+    vote: req.body.playerId
   })
 
 
 });
-  app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
